3082d087afc6d1ce6747118870cf51095ecf55f8
Rommel Lasso <soccerply@msn.com>
2017-10-23 23:01:25 -0400

    Completed Sort

--- simulation.py
+++ simulation.py
@@ -1,1 +1,56 @@
-Hello
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Assignemnt Week Five"""
+
+#import needed libraries
+import random
+import argparse
+from pprint import pprint
+import time
+import decimal
+
+class Printer:
+    def __init__(self, ppm):
+        self.page_rate = ppm
+        self.current_task = None
+        self.time_remaining = 0
+
+    def tick(self):
+        if self.current_task != None:
+            self.time_remaining = self.time_remaining - 1
+            if self.time_remaining <= 0:
+                self.current_task = None
+
+    def busy(self):
+        if self.current_task != None:
+            return True
+        else:
+            return False
+
+    def start_next(self,new_task):
+        self.current_task = new_task
+        self.time_remaining = new_task.get_pages() * 60 / self.page_rate
+
+class Task:
+    def __init__(self, time):
+        self.timestamp = time
+        self.pages = random.randrange(1, 21)
+
+    def get_stamp(self):
+        return self.timestamp
+
+    def get_pages(self):
+        return self.pages
+
+    def wait_time(self, current_time):
+        return current_time - self.timestamp
+
+
+#http: // s3.amazonaws.com / cuny - is211 - spring2015 / requests.csv
+
+def main():
+    print 'hello'
+
+
+if __name__ == '__main__':
+    main()
