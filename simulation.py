4a21525543da5fb0446d1c69fca476f797a03bb6
Rommel Lasso <soccerply@msn.com>
2017-11-13 12:53:19 -0500

    Re-syncing

--- simulation.py
+++ simulation.py
@@ -2,60 +2,26 @@
 # -*- coding: utf-8 -*-
 """Assignemnt Week Five"""
 
-#import needed libraries
-import random
-import urllib2
-import csv
-import argparse
-from pprint import pprint
-import time
-import decimal
+#Import Module that Opens URL Data
+import AnalyzeURL
 
-def downloadData(url):
-    """Downloads data from URL
+class Queue:
+    def __init__(self):
+        self.items = []
 
-    Args:
-        url (string): string value for URL data fetch
+    def is_empty(self):
+        return self.items == []
 
-    Returns:
-        data: something to return
-    """
-    value = urllib2.Request(url)
-    data = urllib2.urlopen(value)
+    def enqueue(self, item):
+        self.items.insert(0,item)
 
-    # print data.read()
-    return data
+    def dequeue(self):
+        return self.items.pop()
 
-def processData(fileContents):
-    """Processes data passed in
+    def size(self):
+        return len(self.items)
 
-    Args:
-        fileContents (object): string value the data file
-
-    Returns:
-        full_database: something to return
-    """
-
-    #print fileContents.read()
-    full_database = {}
-
-    reader = csv.reader(fileContents, delimiter=',')
-
-    for row, item in enumerate(reader):
-        full_database [row] = {
-            'Time_Value': item[0],
-            'File': item[1],
-            'Time_Spent': item[2]
-        }
-
-        print row
-
-    #print 'Image requests account for 45.3% of all requests', img_hit_counter
-    #print fileContents.read()
-
-    return full_database
-
-class Printer:
+class Server:
     def __init__(self, ppm):
         self.page_rate = ppm
         self.current_task = None
@@ -77,7 +43,7 @@ class Printer:
         self.current_task = new_task
         self.time_remaining = new_task.get_pages() * 60 / self.page_rate
 
-class Task:
+class Request:
     def __init__(self, time):
         self.timestamp = time
         self.pages = random.randrange(1, 21)
@@ -94,14 +60,17 @@ class Task:
 def simulateOneServe():
     print 'simulate'
 
-
 def main():
+    # Hardcode URL
+    # Work on Dyanamic URL validator
     url = 'http://s3.amazonaws.com/cuny-is211-spring2015/requests.csv'
-    csvData = downloadData(url)
+    databaseData = AnalyzeURL.downloadData(url)
 
-    databaseData = processData(csvData)
-    #pprint (databaseData)
+    #print (databaseData)
+    print len(databaseData)
 
+    for i in range(0, 10):
+        print databaseData[i]
 
 if __name__ == '__main__':
     main()
