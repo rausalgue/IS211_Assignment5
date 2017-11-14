f1682124167571ed6b9f98e12e90f3d636dfb667
Rommel Lasso <soccerply@msn.com>
2017-11-13 19:27:10 -0500

    Adding dfsdfsdf

--- simulation.py
+++ simulation.py
@@ -2,8 +2,15 @@
 # -*- coding: utf-8 -*-
 """Assignemnt Week Five"""
 
+import argparse
 #Import Module that Opens URL Data
 import AnalyzeURL
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--url", help="URL for data retrieval",default=None)
+parser.add_argument("--servers", type=int, help="Number of Servers", default=None)
+
+args = parser.parse_args()
 
 class Queue:
     def __init__(self):
@@ -22,16 +29,12 @@ class Queue:
         return len(self.items)
 
 class Server:
-    def __init__(self, ppm):
-        self.page_rate = ppm
-        self.current_task = None
+    def __init__(self):
         self.time_remaining = 0
+        self.current_task = None
 
     def tick(self):
-        if self.current_task != None:
-            self.time_remaining = self.time_remaining - 1
-            if self.time_remaining <= 0:
-                self.current_task = None
+        self.current_task = None
 
     def busy(self):
         if self.current_task != None:
@@ -41,24 +44,105 @@ class Server:
 
     def start_next(self,new_task):
         self.current_task = new_task
-        self.time_remaining = new_task.get_pages() * 60 / self.page_rate
+        self.time_remaining = new_task.get_time()
 
 class Request:
-    def __init__(self, time):
+    def __init__(self, time, timeSpent):
         self.timestamp = time
-        self.pages = random.randrange(1, 21)
+        self.timespent = timeSpent
 
     def get_stamp(self):
         return self.timestamp
 
-    def get_pages(self):
-        return self.pages
+    def get_time(self):
+        return self.timespent
 
-    def wait_time(self, current_time):
-        return current_time - self.timestamp
+    def wait_time(self):
+        return self.timespent
 
-def simulateOneServe():
-    print 'simulate'
+def simulateOneServe(requestData):
+    """Similuates process with One Server
+
+    Args:
+        requestData (list): list of processes
+
+    Returns:
+        results: average wait time of server
+    """
+
+    server_queue = Queue()
+
+    waiting_times = []
+
+    server = Server()
+
+    for row in requestData:
+        #print requestData[row]
+        time_value = int(requestData[row]['Time_Value'])
+        time_spent = int(requestData[row]['Time_Spent'])
+
+        request = Request(time_value, time_spent)
+
+        #print 'TimeStamp: ' ,request.timestamp, 'Time Spent on Request: ',request.timespent
+
+        server_queue.enqueue(request)
+
+        if (not server.busy()) and (not server_queue.is_empty()):
+            next_request = server_queue.dequeue()
+            waiting_times.append(next_request.timespent)
+            server.start_next(next_request)
+
+        server.tick()
+
+    #print 'wainting times',sum(waiting_times),len(waiting_times)
+
+    results = sum(waiting_times) / len(waiting_times)
+    return results
+
+def simulateManyServers(requestData,servers):
+    """Similuates process with Multiple Servers
+
+    Args:
+        requestData (list): list of processes
+        servers (int): Number of servers
+
+    Returns:
+        results: average wait time of server
+    """
+    server_queue = Queue()
+    waiting_times = []
+    server = Server()
+
+    server_list = []
+
+    for x in range(servers):
+        #print 'x: ',x
+        server_list.append(Server())
+
+    for k in server_list:
+        #print 'Utilizing Server',
+        for row in requestData:
+            # print requestData[row]
+            time_value = int(requestData[row]['Time_Value'])
+            time_spent = int(requestData[row]['Time_Spent'])
+
+            request = Request(time_value, time_spent)
+
+            # print 'TimeStamp: ' ,request.timestamp, 'Time Spent on Request: ',request.timespent
+
+            server_queue.enqueue(request)
+
+            if (not server.busy()) and (not server_queue.is_empty()):
+                next_request = server_queue.dequeue()
+                waiting_times.append(next_request.timespent)
+                server.start_next(next_request)
+
+            server.tick()
+
+    results = sum(waiting_times) / len(waiting_times)
+    print 'Running on multiple servers: ',servers
+
+    return results
 
 def main():
     # Hardcode URL
@@ -66,11 +150,18 @@ def main():
     url = 'http://s3.amazonaws.com/cuny-is211-spring2015/requests.csv'
     databaseData = AnalyzeURL.downloadData(url)
 
-    #print (databaseData)
-    print len(databaseData)
+    servers = args.servers if args.servers else raw_input("Please provide the Number of Servers to utilize: ")
 
-    for i in range(0, 10):
-        print databaseData[i]
+    if int(servers) > 1:
+        #print 'multiple'
+        #Run Multiple
+        response = simulateManyServers(databaseData,int(servers))
+
+    else:
+        #Run Single
+        response = simulateOneServe(databaseData)
+
+    print("Average Wait %6.2f secs."%(response))
 
 if __name__ == '__main__':
     main()
